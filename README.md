a5a431406d13bc413bd9f64872a1f0b4b051b9fb
The007Programmer <aahils2019@gmail.com>
2025-03-08 15:52:36 -0800

    UNST: add list func

--- README.md
+++ README.md
@@ -16,3 +16,9 @@ Download the ZIP file from the `CODE` tab, then run the shell file.
 
 ### Requirements
 - `JQ` package for json handling
+
+### Commits Key
+- UNST: Unstable commit, not to be used, solely for version control and code backup
+- SAFE: Safe commit, latest code can be used
+- HTFX: Hotfix for critical issue
+- REFA: Refactored/Restructured code for better readability and/or efficiency
--- whitelister.sh
+++ whitelister.sh
@@ -5,32 +5,76 @@
 #sudo ufw enable
 
 
-json_file="db.json"
+# json_file="db.json"
 
-# my dictionary (associative array whatever) with everyone's ips
-declare -A ip_db
-declare -A whitelisted
-declare -A blacklisted
+# # my dictionary (associative array whatever) with everyone's ips
+# declare -A ip_db
+# declare -A whitelisted
+# declare -A blacklisted
 
-# loops through each key-value pair in the JSON and populate the associative array
-# used CHATGPT for some syntax help with formatting the json content.
-while IFS="=" read -r key value; do
-    ip_db["$key"]="$value"
-done < <(jq -r 'to_entries[] | "\(.key)=\(.value)"' "$json_file")
+# # loops through each key-value pair in the JSON and populate the associative array
+# # used CHATGPT for some syntax help with formatting the json content.
+# while IFS="=" read -r key value; do
+#     ip_db["$key"]="$value"
+# done < <(jq -r 'to_entries[] | "\(.key)=\(.value)"' "$json_file")
 
 
-# for loop silly syntax
-for ip in "${!ip_db[@]}"; do
-    # also this sudo line allows each i term to connect to port 25565 with ufw    
-    #sudo ufw allow from "$ip" to any port 25565 proto tcp
-    echo "$ip"
+# # for loop silly syntax
+# for person in "${!ip_db[@]}"; do
+#     # also this sudo line allows each i term to connect to port 25565 with ufw    
+#     #sudo ufw allow from "$ip" to any port 25565 proto tcp
+
+#     # 
+#     echo "$person has been whitelisted with ip: ${ip_db[$person]}"
+#     whitelisted["$ip"]="${ip_db[$person]}"
     
-done
+# done
 
 
+main() {
+    read -p "Enter CMD:  |  LIST (a)  |  SHOWLIST (l)  |  SEARCH (s)  |  REMOVE (r)  |: " cmd
 
+    if [[ "$cmd" == "a" ]]; then
+        read -p "Enter player name: " name
+        read -p "Enter player IP: " ip
+        read -p "Whitelist or Blacklist (w/b): " w_b 
+        list "$name" "$ip" "$w_b"
+    fi
 
+}
 
+list() {
+    # Used chatgpt for the json syntax !!!
+    local player="$1"
+    local ip="$2"
+    local whiteblack="$3"
+
+    if [[ "$3" == "w"]]
+        
+        # sudo ufw allow from "$ip" to any port 25565 proto tcp
+        
+        # adds to whitelisted database
+        jq --arg player "$player" --arg ip "$ip" '.whitelisted[$player] = $ip' db.json > temp.json && mv temp.json db.json
+        
+        # success message
+        echo "Successfully whitelisted $player with IP: $ip"
+    
+    if [[ "$3" == "b"]]
+    
+        # sudo ufw deny from "$ip" to any port 25565 proto tcp
+        
+        # adds to blacklisted database
+        jq --arg player "$player" --arg ip "$ip" '.blacklisted[$player] = $ip' db.json > temp.json && mv temp.json db.json
+        
+        # success message
+        echo "Successfully whitelisted $player with IP: $ip"
+    
+    elif [[ "$3" != "w" || "$3" != "b" ]]
+        echo "Try again!"
+        main
+}
+
+main
 
 
 # once all ips are added it'll then deny all other ips attempting to connect
@@ -41,4 +85,3 @@ done
 
 # reloads all settings into effect
 #sudo ufw reload
-
