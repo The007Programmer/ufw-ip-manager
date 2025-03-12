66411274f9dc6fcd7a86454931d63ecdd897d2b8
The007Programmer <aahils2019@gmail.com>
2025-03-12 11:17:52 -0700

    SAFE: added show_db, updated code structure

--- tool.py
+++ tool.py
@@ -1,9 +1,8 @@
 import os
-import subprocess
 import json
 
 def main():
-    start=input("ENTER CMD #:\n1) MANAGE Players (Add/Remove/Switch)\n2) SHOW Whitelist/Blacklist\n3) SEARCH for a player\n4) EXIT\n\n")
+    start = input("ENTER CMD #:\n1) MANAGE Users (Add/Remove/Switch)\n2) SHOW Whitelist/Blacklist\n3) SEARCH for a user\n4) EXIT\n\n")
     if start == '1':
         manager()
     if start == '2':
@@ -12,34 +11,29 @@ def main():
         search()
     if start == '4':
         exit()
-        
 
 def manager():
-    # takes player name, ip, white/black list, does the sudo ufw allow ip, then returns confirm message, asks if more players to add
-    name=input("Player Name? ")
-    ip=input("Player IP? ")
-    w_b=input("Whitelist or Blacklist? (w/b) ")
-    
+    name = input("\nUser Name? ")
+    ip = input("User IP? ")
+    w_b = input("Whitelist or Blacklist? (w/b) ")
+
     if w_b not in ["w", "b"]:
         print("\nPlease specify white/black list!")
         manager()
 
-    # Load the database file if it exists, otherwise create a new database
     db_file = 'db.json'
 
-    # if file exists in path then open as read, if not create local db
-    if os.path.exists(db_file):
-        with open(db_file, 'r') as file:
-            db = json.load(file)
-    else:
-        db = {"whitelist": {}, "blacklist": {}}
+    if not os.path.exists(db_file):
+        print(f"\nDatabase file '{db_file}' does not exist in the current directory.\n")
+        return
 
-    # Check if the player is already in the whitelist or blacklist
+    with open(db_file, 'r') as file:
+        db = json.load(file)
+
     if name in db["whitelist"] and db["whitelist"][name] == ip:
         if w_b == 'w':
             print(f"\n{name} is already in the whitelist.\n")
         else:
-            # Ask if the user wants to switch the player to the blacklist
             switch = input(f"{name} is in the whitelist. Do you want to switch to blacklist? (y/n) ")
             if switch == 'y':
                 db["whitelist"].pop(name)
@@ -50,14 +44,12 @@ def manager():
         if w_b == 'b':
             print(f"\n{name} is already in the blacklist.\n")
         else:
-            # Ask if the user wants to switch the player to the whitelist
             switch = input(f"{name} is in the blacklist. Do you want to switch to whitelist? (y/n) ")
             if switch == 'y':
                 db["blacklist"].pop(name)
                 db["whitelist"][name] = ip
                 print(f"\n{name} has been moved to the whitelist.\n")
     else:
-        # Add the player to the specified list
         if w_b == 'w':
             db["whitelist"][name] = ip
             print(f"\n{name} has been added to the whitelist.\n")
@@ -65,12 +57,10 @@ def manager():
             db["blacklist"][name] = ip
             print(f"\n{name} has been added to the blacklist.\n")
 
-    # Save the updated database back to the file
     with open(db_file, 'w') as file:
         json.dump(db, file, indent=2)
 
-    # Ask if the user wants to add more players
-    more = input("Do you want to add more players? (y/n) ")
+    more = input("Do you want to add more users? (y/n) ")
     if more == 'y':
         manager()
     else:
@@ -78,7 +68,34 @@ def manager():
         main()
 
 def show_db():
-    pass
+    db_file = 'db.json'
+
+    if not os.path.exists(db_file):
+        print("\nReturning to menu!\n")
+        main()
+
+def show_db():
+
+    # Load the database file if it exists, otherwise create a new database
+    db_file = 'db.json'
+    # if file exists in path then open as read, if not create local db
+    if os.path.exists(db_file):
+        with open(db_file, 'r') as file:
+            db = json.load(file)
+
+    which=input("\nWhich database would you like to see? Whitelist, Blacklist or All? (w/b/a) ")
+    match which:
+        case "w":
+            print("\n===== WHITELIST =====")
+            print(json.dumps(db.get("whitelist", {}), indent=4))
+            return1=input("\nPress  key to continue...\n")
+        case "b":
+            print("\n===== BLACKLIST =====")
+            print(json.dumps(db.get("blacklist", {}), indent=4))
+        case "a":
+            print("\n===== ALL LISTS =====")
+            print(json.dumps(db, indent=4))
+            
 
 def search():
     pass
